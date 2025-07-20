c51d9880c4f4631f6b67fa7cd2285524ea393785
TheDeathlyCow <thedeathlycow@gmail.com>
2025-07-20 04:10:31 -1000

    create browser widget class

---
+++ WebToCPPTest/Source/WebToCPPTest/UBrowserWidget.cpp
@@ -0,0 +1,33 @@
+﻿#include "UBrowserWidget.h"
+
+void UBrowserWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (!WebBrowser)
+	{
+		UE_LOG(LogTemp, Error, TEXT("WebBrowser not bound!"));
+		return;
+	}
+
+	WebBrowser->ExecuteJavascript(TEXT("console.log('Hello from C++!');"));
+
+	WebBrowser->LoadURL(GetLocalHtmlPath());
+
+	WebBrowser->OnUrlChanged.AddDynamic(this, &UBrowserWidget::HandleUrlChanged);
+}
+
+void UBrowserWidget::HandleUrlChanged(const FText& ButtonText)
+{
+	if (WebBrowser)
+	{
+		FString JS = TEXT("console.log('C++ saw you changed url :o');");
+		WebBrowser->ExecuteJavascript(JS);
+	}
+}
+
+FString UBrowserWidget::GetLocalHtmlPath() const
+{
+	FString AbsolutePath = FPaths::ConvertRelativePathToFull(FPaths::ProjectContentDir() / TEXT("UI/index.html"));
+	return FString::Printf(TEXT("file:///%s"), *AbsolutePath.Replace(TEXT("\\"), TEXT("/")));
+}
---
+++ WebToCPPTest/Source/WebToCPPTest/UBrowserWidget.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "WebBrowser.h"
+#include "UBrowserWidget.generated.h"
+
+UCLASS()
+class UBrowserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void NativeConstruct() override;
+
+public:
+	UPROPERTY(meta = (BindWidget))
+	UWebBrowser* WebBrowser;
+
+private:
+	UFUNCTION()
+	void HandleUrlChanged(const FText& ButtonText);
+
+	UFUNCTION()
+	FString GetLocalHtmlPath() const;
+};
